cf67132674911e33d6b628f9b06a4171ae6d411e
zakkturner <30674356+zakkturner@users.noreply.github.com>
2020-07-03 15:56:02 -0400

    made responsive

--- src/com/nav/Nav.js
+++ src/com/nav/Nav.js
@@ -14,7 +14,13 @@ export default function Nav() {
     <nav className="nav">
       <div className="nav__grid">
         <h3 className="nav__grid__logo">ZT</h3>
-
+        <div className="nav__grid__hamburgercontainer">
+          <div className="nav__grid__hamburgercontainer__hamburger">
+            <a>
+              <div className="nav__grid__hamburgercontainer__hamburger--inner"></div>
+            </a>
+          </div>
+        </div>
         <ul className="nav__grid__links">
           <li>
             <a href="#" className="nav__grid__links-item">
